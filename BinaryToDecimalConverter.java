322e7c3781e484b46c82e10ee0811b5c1701b9c1
Ganga Gandrakota <147605390+GangaGandrakota@users.noreply.github.com>
2023-12-06 15:52:54 +0530

    Add files via upload

---
+++ BinaryToDecimalConverter.java
@@ -0,0 +1,26 @@
+package BinaryToDecimalConverterProject;
+
+import java.util.Scanner;
+public class BinaryToDecimalConverter 
+{
+	public static void main(String[] args) 
+	{
+		        Scanner scanner = new Scanner(System.in);
+
+		        System.out.print("Enter a binary number: ");
+		        String binaryNumber = scanner.next();
+
+		        try {
+		            long decimalNumber = binaryToDecimal(binaryNumber);
+		            System.out.println("Decimal equivalent: " + decimalNumber);
+		        } catch (NumberFormatException e) {
+		            System.out.println("Invalid binary number format.");
+		        }
+
+		        scanner.close();
+		    }
+
+		    public static long binaryToDecimal(String binaryNumber) {
+		        return Long.parseLong(binaryNumber, 2);
+		    }
+		}
